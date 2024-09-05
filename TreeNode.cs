138fdc7a7d81a9720d5171a1792872254e9c24bf
Michael Swisa <ms0504109182@gmail.com>
2024-09-05 11:35:39 +0300

    Add project files.

---
+++ TreeNode.cs
@@ -0,0 +1,20 @@
+﻿namespace TestDataStructures
+{
+    internal class TreeNode
+    {
+        public int MinSeverity { get; set; }
+        public int MaxSeverity { get; set; }
+        public List<string> Defenses { get; set; }
+        public TreeNode Right { get; set; }
+        public TreeNode Left { get; set; }
+
+        public TreeNode(int minSeverity, int maxSeverity, List<string> defenses)
+        {
+            this.MinSeverity = minSeverity;
+            this.MaxSeverity = maxSeverity;
+            this.Defenses = defenses;
+            this.Right = null;
+            this.Left = null;
+        }
+    }
+}
